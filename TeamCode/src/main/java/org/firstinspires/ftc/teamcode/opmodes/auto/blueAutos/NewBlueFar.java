61ad9555a10f0d3018c2506a240cfcd35b2a90b1
FTC Team Boltic 24108 <145214158+bolticteam@users.noreply.github.com>
2026-01-16 18:12:24 -0600

    changed stuff for spindexer in auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/blueAutos/NewBlueFar.java
@@ -0,0 +1,284 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto.blueAutos;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.math.MathFunctions;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.subsystems.AprilTagStuff;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.TestDexer;
+import org.firstinspires.ftc.teamcode.subsystems.Turret;
+import org.firstinspires.ftc.teamcode.subsystems.testShooter;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+@Autonomous(name = "blue far test", group = "Autonomous")
+public class NewBlueFar extends LinearOpMode {
+
+    Follower follower;
+    ElapsedTime spindexerDelayTimer = new ElapsedTime();
+
+    ElapsedTime runtime = new ElapsedTime();
+    testShooter shooter = new testShooter();
+    Intake intake = new Intake();
+    TestDexer testDexer = new TestDexer();
+    Turret turret = new Turret();
+    AprilTagStuff aprilTagStuff = new AprilTagStuff();
+    // Paths
+    PathChain preload, set2, grab2, emptyRamp, shoot2, set3, grab3, shoot3, set4, grab4, shoot4, park;
+
+    int step = 0;
+    int stateVar = 0;
+    double targetVel = 0;
+    double goalX = 0;
+    double goalY = 144;
+    double goalDist;
+    double goalAngle;
+    String order = "null";
+    double disX;
+    double disY;
+    String intakeSide = "front";
+    int ballCount = 3;
+    boolean launched = false;
+
+    public double robotHeading;
+
+    int desiredTurretAngle;
+
+    boolean grabbing;
+
+    @Override
+    public void runOpMode() {
+        int spinPos;
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(new Pose(56, 8,  Math.toRadians(180)));
+        turret.init(hardwareMap);
+        testDexer.init(hardwareMap);
+        shooter.init(hardwareMap, telemetry);
+        intake.init(hardwareMap);
+        aprilTagStuff.init(hardwareMap, telemetry);
+
+        buildPaths();   // ← ONLY builds, does NOT run anything
+        ElapsedTime runtime = new ElapsedTime();
+        testDexer.currentOrder = "PPG";
+        //init sequence - get servios up to speed, reset pos, set up for shot
+        while(runtime.seconds() < 3){
+            testDexer.s1.setPower(-1);
+            testDexer.s2.setPower(-1);
+            telemetry.addLine("WAIT");
+        }
+        spinPos = testDexer.updatePos();
+        while (spinPos < testDexer.getTargetPos() -150){
+            spinPos = testDexer.updatePos();
+            testDexer.setPowerToPosition2(spinPos, runtime.seconds());
+            telemetry.addData("pose", spinPos);
+        }
+
+        grabbing = false;
+        testDexer.s1.setPower(0);
+        testDexer.s2.setPower(0);
+        testDexer.currentOrder = "PPG";
+        telemetry.addLine("Ready for start");
+        telemetry.update();
+
+
+        ElapsedTime aprilTimer = new ElapsedTime();
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        // Start first path
+        follower.followPath(preload);
+        runtime.reset();
+        aprilTimer.reset();
+        runtime.reset();
+        spindexerDelayTimer.reset();
+        while (opModeIsActive()) {
+            disX = goalX - follower.getPose().getX();
+            disY = goalY - follower.getPose().getY();
+            robotHeading = follower.getHeading(); //will always be something plus that starting of 90
+            double turretXOffset = 3.175 * Math.cos((robotHeading));
+            double turretYOffset = 3.175*Math.sin((robotHeading));
+            disX += turretXOffset;
+            disY += turretYOffset;
+            goalDist = Math.sqrt(Math.pow(disX, 2) + Math.pow(disY, 2)); //pythagorean theorem
+            goalAngle = Math.abs(Math.atan2(disX, disY)) + Math.toRadians(90); //simple inverse trig with compensation for robot's extra 90 degrees
+            desiredTurretAngle = turret.calculateTurnBlue(goalAngle, robotHeading);
+
+            spinPos = testDexer.updatePos();
+            testDexer.setPowerToPosition2(spinPos, runtime.seconds());
+            follower.update();
+            switch (testDexer.getSpinState()){
+                case MOVE_TO_INTAKE:
+                    if(intakeSide.equals("front")){
+                        testDexer.spinToFront(ballCount);
+                        testDexer.setSpinState(2);
+                    }else{
+                        testDexer.spinToBack(ballCount);
+                        testDexer.setSpinState(2);
+
+                    }
+                case INTAKING:
+                    //if intaking and not full, check for color. if color, move to next slot. repeat until full.
+                    if(ballCount < 3){
+                        testDexer.checkForBalls();
+                        if((testDexer.checkForColorAtSpot('P', ballCount +1) || testDexer.checkForColorAtSpot('G', ballCount +1)) && spindexerDelayTimer.seconds() > .5) { //wait until color
+                            ballCount++;
+                            testDexer.spinToNext(intakeSide);
+                            spindexerDelayTimer.reset();
+                        }
+                    }
+                    else{
+                        testDexer.setSpinState(3);
+                    }
+                case PREPARE_FOR_SHOT:
+                    testDexer.setUpForShooting(order);
+                    testDexer.setSpinState(0);
+                case SHOOT:
+                    if(shooter.getLauunchState() == testShooter.LaunchState.LAUNCH && !launched) {
+                        //testDexer.shoot(); re-add later
+                        testDexer.s1.setPower(.2);
+                        testDexer.s2.setPower(.2);
+                        launched = true;
+                    }
+                    if(shooter.getLauunchState() == testShooter.LaunchState.RECOVER){
+                        testDexer.setSpinState(0);
+                        testDexer.s1.setPower(0);
+                        testDexer.s2.setPower(0);
+                        ballCount = 0;
+                        launched = false;
+                    }
+                //if idle just dont change anything
+            }
+            switch (step) {
+                //step 1 - shoot3
+                case 0:
+                    shooter.shoot3();
+                    step++;
+                    break;
+                case 1:
+                    if(shooter.isActive){
+                        shooter.updateState(targetVel);
+                        if(shooter.getLauunchState() == testShooter.LaunchState.LAUNCH && testDexer.getSpinState() == TestDexer.SpinState.SHOOT && !launched){
+                            testDexer.setSpinState(4);
+                        }
+                        break;
+                    }
+                    testDexer.setSpinState(0);
+                    step++;
+                    break;
+
+                case 2:
+                    if(!follower.isBusy()){
+                        follower.followPath(park);
+                        step++;
+                    }
+                    break;
+
+//                case 4:
+//                    if (!follower.isBusy()) {
+//                        follower.followPath(park);
+//                        step++;
+//                    }
+//                    break;
+
+
+            }
+
+            // Debug Info
+            telemetry.addData("Step", step);
+            telemetry.addData("Pose", follower.getPose());
+            telemetry.addData("Busy", follower.isBusy());
+            telemetry.addData("spindexer pos", spinPos);
+            telemetry.addData("target", testDexer.getTargetPos());
+            telemetry.update();
+        }
+    }
+
+    // ---------------------------------------------------------------------
+    // PATH GENERATION
+    // ---------------------------------------------------------------------
+    public void buildPaths() {
+
+        final Pose startPose = new Pose(56, 8,  Math.toRadians(180));
+        final Pose shootMid = new Pose(60, 8,  Math.toRadians(180));
+        final Pose shootMid2 = new Pose(50, 50,  Math.toRadians(180));
+        final Pose shootPoseFar = new Pose(60, 16, Math.toRadians(180)); //Change Coordinates
+        final Pose grabPose1 = new Pose(40, 60, Math.toRadians(180));
+        final Pose grabbed1 = new Pose(23, 60, Math.toRadians(180));
+        final Pose rampMid = new Pose(11, 67, Math.toRadians(225));
+        final Pose ramp = new Pose(15, 70, Math.toRadians(180));
+        final Pose grabPose2 = new Pose(40, 36, Math.toRadians(180));
+        final Pose grabbed2 = new Pose(23, 36, Math.toRadians(180));
+        final Pose grabPose3 = new Pose(32, 16, Math.toRadians(180));
+        final Pose grabbed3 = new Pose(10, 16, Math.toRadians(180));
+        final Pose parkPose = new Pose(24, 70,   Math.toRadians(180));
+
+        preload = follower.pathBuilder()
+                .addPath(new BezierCurve(startPose, shootMid, shootPoseFar))
+                .setLinearHeadingInterpolation(startPose.getHeading(), shootMid.getHeading(), shootPoseFar.getHeading())
+                .addParametricCallback(0.75, () -> {shooter.shoot3();})
+                .build();
+
+        set2 = follower.pathBuilder()
+                .addPath(new BezierLine(shootPoseFar, grabPose1))
+                .setLinearHeadingInterpolation(shootPoseFar.getHeading(), grabPose1.getHeading())
+                .build();
+
+        grab2 = follower.pathBuilder()
+                .addPath(new BezierLine(grabPose1, grabbed1))
+                .setLinearHeadingInterpolation(grabPose1.getHeading(), grabbed1.getHeading())
+                .setVelocityConstraint(5)
+                .build();
+        emptyRamp = follower.pathBuilder()
+                .addPath(new BezierCurve(grabbed1, rampMid, ramp))
+                .setLinearHeadingInterpolation(grabbed1.getHeading(), rampMid.getHeading(), ramp.getHeading())
+                .setVelocityConstraint(5)
+                .build();
+        shoot2 = follower.pathBuilder()
+                .addPath(new BezierCurve(ramp, shootMid2, shootPoseFar))
+                .setLinearHeadingInterpolation(ramp.getHeading(), shootMid2.getHeading(), shootPoseFar.getHeading())
+                .addParametricCallback(0.75, () -> {shooter.shoot3();})
+                .build();
+        set3 = follower.pathBuilder()
+                .addPath(new BezierLine(shootPoseFar, grabPose2))
+                .setLinearHeadingInterpolation(shootPoseFar.getHeading(), grabPose2.getHeading())
+                .build();
+
+        grab3 = follower.pathBuilder()
+                .addPath(new BezierLine(grabPose2, grabbed2))
+                .setLinearHeadingInterpolation(grabPose2.getHeading(), grabbed2.getHeading())
+                .build();
+
+        shoot3 = follower.pathBuilder()
+                .addPath(new BezierLine(grabbed2, shootPoseFar))
+                .setLinearHeadingInterpolation(grabbed2.getHeading(), shootPoseFar.getHeading())
+                .addParametricCallback(0.75, () -> {shooter.shoot3();})
+                .build();
+        set4 = follower.pathBuilder()
+                .addPath(new BezierLine(shootPoseFar, grabPose3))
+                .setLinearHeadingInterpolation(shootPoseFar.getHeading(), grabPose3.getHeading())
+                .build();
+        grab4 = follower.pathBuilder()
+                .addPath(new BezierLine(grabPose3, grabbed3))
+                .setLinearHeadingInterpolation(grabPose3.getHeading(), grabbed3.getHeading())
+                .build();
+        shoot4 = follower.pathBuilder()
+                .addPath(new BezierLine(grabbed3, shootPoseFar))
+                .setLinearHeadingInterpolation(grabbed3.getHeading(), shootPoseFar.getHeading())
+                .addParametricCallback(0.75, () -> {shooter.shoot3();})
+                .build();
+        park = follower.pathBuilder()
+                .addPath(new BezierLine(shootPoseFar, parkPose))
+                .setLinearHeadingInterpolation(shootPoseFar.getHeading(), parkPose.getHeading())
+                .build();
+    }
+}
+
